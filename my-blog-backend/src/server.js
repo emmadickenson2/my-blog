1690dda1f1fbb43c4ec8898469fb9bf5daac9f2e
emmadickenson2 <emma.dickenson@wsu.edu>
2023-12-08 08:46:44 -0800

    practice with requests

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,11 +1,13 @@
 import express from 'express'
 
-const app = express()
+const app = express();
+app.use(express.json());
 
-app.get('/hello', (req, res) => {
-    res.send('Hello!')
-})
+app.post('/hello', (req, res) => {
+    console.log(req.body);
+    res.send(`Hello ${req.body.name}!`);
+});
 
 app.listen(8000, () => {
     console.log('Server is listening on port 8000')
-})
+});
