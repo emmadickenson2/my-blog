bee56741c7ed6d10bcc42d009e3ad781478bc5f4
emmadickenson2 <emma.dickenson@wsu.edu>
2023-12-12 09:48:51 -0800

    display upvotes

--- blog/src/pages/ArticlePage.js
+++ blog/src/pages/ArticlePage.js
@@ -1,9 +1,16 @@
-import { useParams } from 'react-router-dom'
-import articles from './article-content'
-import NotFoundPage from './NotFoundPage'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import articles from './article-content';
+import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = () => {
-    const { articleId } = useParams()
+    const [articleInfo, setArticleInfo] = useState( { upvotes: 0, comments: [] } );
+
+    useEffect(() => {
+        setArticleInfo({ upvotes: 3, comments: [] });
+    });
+
+    const { articleId } = useParams();
     const article = articles.find(article => article.name === articleId)
 
     if (!article) {
@@ -13,6 +20,7 @@ const ArticlePage = () => {
     return (
         <>
         <h1>{article.title}</h1>
+        <p>This article has { articleInfo.upvotes } upvote(s)</p>
         {article.content.map((paragraph, i) => (
             <p key={i}>{paragraph}</p>
         ))}
