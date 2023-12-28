2bb7535573f7609bc22e86c4a5441e2307184d7d
emmadickenson2 <emma.dickenson@wsu.edu>
2023-12-28 12:42:20 -0800

    added base pages for login and create account

--- blog/src/index.js
+++ blog/src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCAYM6K7tNMQx8x44x2XdCAiGC2HsjzUzk",
+  authDomain: "my-blog-6c86a.firebaseapp.com",
+  projectId: "my-blog-6c86a",
+  storageBucket: "my-blog-6c86a.appspot.com",
+  messagingSenderId: "117367939655",
+  appId: "1:117367939655:web:8299f18d78946beeb8c762"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
