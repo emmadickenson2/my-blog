b7b279296f9cbe3fd05e592c96a6c2e9a3783b33
emmadickenson2 <emma.dickenson@wsu.edu>
2023-12-29 13:24:54 -0800

    added login and ability to like and comment only when logged in

--- blog/src/pages/ArticlePage.js
+++ blog/src/pages/ArticlePage.js
@@ -8,25 +8,32 @@ import useUser from '../hooks/useUser';
 import AddCommentForm from '../components/AddCommentForm';
 
 const ArticlePage = () => {
-    const [articleInfo, setArticleInfo] = useState( { upvotes: 0, comments: [] } );
+    const [articleInfo, setArticleInfo] = useState( { upvotes: 0, comments: [], canUpvote: false } );
+    const { canUpvote } = articleInfo;
     const { articleId } = useParams();
 
     const { user, isLoading } = useUser()
 
     useEffect(() => {
         const loadArticleInfo = async () => {
-            const response = await axios.get(`/api/articles/${articleId}`);
+            const token = user && await user.getIdToken();
+            const headers = token ? { authtoken: token } : {};
+            const response = await axios.get(`/api/articles/${articleId}`, {headers });
             const newArticleInfo = response.data;
             setArticleInfo(newArticleInfo);
         }
 
-        loadArticleInfo();
-    }, []);
+        if (isLoading) {
+            loadArticleInfo();
+        }
+    }, [isLoading, user]);
 
     const article = articles.find(article => article.name === articleId);
 
     const addUpvote = async () => {
-        const response = await axios.put(`/api/articles/${articleId}/upvote`)
+        const token = user && await user.getIdToken();
+        const headers = token ? { authtoken: token } : {};
+        const response = await axios.put(`/api/articles/${articleId}/upvote`, null, { headers })
         const updatedArticle = response.data;
         setArticleInfo(updatedArticle);
     }
@@ -40,7 +47,7 @@ const ArticlePage = () => {
         <h1>{article.title}</h1>
         <div className='upvotes-section'>
             {user
-                ? <button onClick={addUpvote}>Upvote</button>
+                ? <button onClick={addUpvote}>{canUpvote ? 'Upvote': 'Already Upvoted'}</button>
                 : <button>Log in to upvote</button>}
             <p>This article has { articleInfo.upvotes } upvote(s)</p>
         </div>
@@ -51,7 +58,7 @@ const ArticlePage = () => {
             ? <AddCommentForm
             articleName={articleId}
             onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
-            : <button>Login to add a comment</button>}
+            : <button>Log in to add a comment</button>}
         <CommentsList comments ={articleInfo.comments} />
         </>
     );
