61a0986d559bdbe634edf0e0e7454191cc82e092
emmadickenson2 <emma.dickenson@wsu.edu>
2024-01-02 14:37:24 -0800

    prepared app for deployment

--- my-blog-backend/src/db.js
+++ my-blog-backend/src/db.js
@@ -2,7 +2,8 @@ import { MongoClient } from 'mongodb';
 
 let db;
 async function connectToDb(cb) {
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    
+    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.fhhvavu.mongodb.net/?retryWrites=true&w=majority`);
     await client.connect();
 
     db = client.db('react-blog-db');
